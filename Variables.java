2b56835c1a4a837ea93071289e7c4fe053b51086
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-01-09 18:25:09 +0530

    first commit

---
+++ Variables.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+
+public class Variables {
+    public static void main(String[] args){
+        var phoneNum = Long.parseLong("7891916296");
+        String name = "kamal bhera";
+        Boolean developer = true;
+
+        System.out.println(phoneNum);
+        System.out.println(name);
+        System.out.println(developer);
+
+
+    }
+}
