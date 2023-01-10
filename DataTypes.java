dcb0d2fa7a35020593ba07a8a089ffa1936de0c5
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-01-10 11:32:54 +0530

    datatypes

---
+++ DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // Java have two types of data type
+        Boolean one = false;
+        // Byte Data Type
+        byte byt1 = 10, byt2 = -20;
+        // Short Data Type
+        short s = 10000;
+        short r = -5000;
+        // Int Data Type
+        int num = 100000;
+        int num2 = -200000;
+        // Long Data Type
+        long a = 100000L;
+        long b = -200000L;
+        // Float Data Type
+        float f1 = 234.5f;
+        // Double Data Type
+        double d1 = 12.3;
+        // Char Data Type
+        char letterA = 'A';
+    }
+}
